db2cb89f430a9692dfe2cb029c2e1c0aea0865ee
Rafael Moal <r.moal@student.isartdigital.com>
2025-02-24 23:46:55 +0100

    Update : Ball can fall and collide with ground

--- Assets/Scripts/Game/Ball/BallGravity.cs
+++ Assets/Scripts/Game/Ball/BallGravity.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class BallGravity : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float _Gravity = -9.81f;
+    [SerializeField] private float _Mass = 10f;
+    [SerializeField] private float _RaycastDistance = 3.1f;
+    [SerializeField] private LayerMask _GroundLayer;
+
+    private float _FallForce = 0f;    
+    private RaycastHit _Hit;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Debug.DrawRay(transform.position, Vector3.down * _RaycastDistance, Color.black);
+        if (Physics.Raycast(transform.position, Vector3.down, out _Hit, _RaycastDistance, _GroundLayer))
+        {
+            _FallForce = 0f;
+            transform.position = new Vector3(transform.position.x, _Hit.point.y + transform.localScale.y / 2, transform.position.z);
+        }
+        else
+        {
+            _FallForce += _Gravity * _Mass * Time.deltaTime;
+            transform.position += Vector3.up * _FallForce * Time.deltaTime;
+        }
     }
+
+    
 }
