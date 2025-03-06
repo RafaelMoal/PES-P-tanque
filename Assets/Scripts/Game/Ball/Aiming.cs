ff8bd568fbb34d67cbaed74013fbfda57c060c96
Rafael Moal <r.moal@student.isartdigital.com>
2025-03-06 19:06:54 +0100

    Added : Aiming.cs

---
+++ Assets/Scripts/Game/Ball/Aiming.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aiming : MonoBehaviour
+{
+    [Space(10), Header("Gameobjects")]
+    [SerializeField] private  BallManager _BallManager;
+    [SerializeField] private Throw _Throw;
+    [SerializeField] private BallGravity _BallGravity;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
